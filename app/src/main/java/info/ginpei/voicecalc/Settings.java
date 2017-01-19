7c427755e182cdf46788409f1dcc4ed06b93dca7
Ginpei <ginpei@gmail.com>
2017-01-19 15:54:56 -0800

    make a field private

--- app/src/main/java/info/ginpei/voicecalc/Settings.java
+++ app/src/main/java/info/ginpei/voicecalc/Settings.java
@@ -6,7 +6,7 @@ public class Settings {
     final public static String NAME = "info.ginpei.voicecalc.SETTINGS";
     final public static String KEY_AUTO_EQUALS = "autoEquals";
 
-    SharedPreferences preferences;
+    private SharedPreferences preferences;
     private SharedPreferences.Editor editor;
 
     public Settings(SharedPreferences preferences) {
